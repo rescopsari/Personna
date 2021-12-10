5a6f126b461592c6f1c4b95aef69c982dea474e1
Jason Richard <rescopsari.master@gmail.com>
2021-12-10 11:42:34 +0100

    rename

--- Python/__main__.py
+++ Python/__main__.py
@@ -1,5 +1,7 @@
 from personna import *
-from test import *
+from personna_test import *
+
+import os
 
 def Launch_list_habitant():
     for element in range(0, 3 + 1):
@@ -17,12 +19,15 @@ def Launch_list_habitant():
         Pointeur.show_address()
         print("--")
 
+os.system("cls") 
 answer = input("Test ou Start : ")
 while answer != "Test" or answer != "Start":
     if answer == "Test":
+        os.system("cls") 
         PointerTest = TestPersonna()
         break
     elif answer == "Start":
+        os.system("cls") 
         Launch_list_habitant()
         break
     answer = input("Test ou Start : ") 
