bf8e6bd93970baca4877981ebe05e8a02bd70ba6
Jason Richard <rescopsari.master@gmail.com>
2021-12-10 14:37:16 +0100

    unittest

--- Python/personna_test.py
+++ Python/personna_test.py
@@ -1,14 +1,25 @@
 from personna import *
+import unittest
 
 class TestPersonna:
     def __init__(self):
-        Test = Personna("Test prenom", "Test nom")
         Pointeur = Personna("Theo", "La remorque")
         Pointeur.set_address("10", "rue de la betonniere", "Grenoble", "35010")
-        if isinstance(Test, object):
+        if isinstance(Pointeur, object):
             print("Object has been initialized")
             print(Pointeur.__str__())
             Pointeur.show_address()
             print("--")
         else:
             print("[ERROR] Object has not been initialized")
+
+class PersonnaTest(unittest.TestCase):
+    def test_initialization_object(self):
+        Test = Personna("Theo", "La remorque")
+        self.assertEqual(Test.__str__(), "Hi ! I'm Theo La remorque")
+    
+    def test_set_address(self):
+        Test = Personna("Theo", "La remorque")
+        Test.set_address("10", "rue de la betonniere", "Grenoble", "35010")
+        self.assertEqual(Test.show_address(), "My full address is : 10 rue de la betonniere, Grenoble (35010)")
+
